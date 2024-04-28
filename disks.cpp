524629566cbcc92db95910708ebb7e2eaec6f5c1
FerDesaM <fdezaso@unsa.edu.pe>
2024-04-28 18:52:59 -0500

    future implementation of a disk

---
+++ disks.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+int main(){
+    
+        
+    cout<<"FUTURO DISCO A USAR";
+    return 0;
+}
