d73481fdd2c5a7c37eb583d8915b195a8d1d4025
FerDesaM <fdezaso@unsa.edu.pe>
2024-04-30 10:21:46 -0500

    First idea of the disk

--- disks.cpp
+++ disks.cpp
@@ -4,9 +4,36 @@
 #include <algorithm>
 #include <sstream>
 using namespace std;
+class Disk{
+private:
+    string a;
+public:
+    Disk(int a,int b,int c,int d);
+};
+class Plates{
+private:
+public:
+
+};
+class Surfaces{
+private:
+public:
+};
+class Sector{
+private:
+public:
+};
+class Track{
+private:
+public:
+
+};
+class Block{
+
+};
 int main(){
-    
-        
+
+//Disk disk(1,2,3,4);
     cout<<"FUTURO DISCO A USAR";
     return 0;
 }
